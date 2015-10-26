744fbeb01d262d9916cb43ee561cf3b2e36b54ca
Bob Williams <bobwilliams.ii@gmail.com>
2015-10-26 14:57:27 -0400

    initial add

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject palindrome "0.1.0-SNAPSHOT"
+  :description "Clojure implementation of SPARC's palindrome test"
+  :url "https://github.com/bobwilliams"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :main palindrome.core)
---
+++ src/palindrome/core.clj
@@ -0,0 +1,48 @@
+(ns palindrome.core)
+
+(def usage "Usage: <start-date> <end-date>.  Where start-date and end-date should be in 'MMddyyyy' format and start-date must be before, or on, end-date.")
+
+(defn two-args? [args]
+  (= 2 (count args)))
+
+(defn time-to-stop [start end]
+  (= 1 (compare start end)))
+
+(defn get-date-obj [date]
+  "Takes a date string and turns it into a java.util.Date"
+  (.parse (java.text.SimpleDateFormat. "MMddyyyy") date))
+
+(defn get-date-str [date]
+  "Takes a java.util.Data and converts it to a string representation"
+  (.format (java.text.SimpleDateFormat. "MMddyyyy") date))
+
+(defn add-one-day [date]
+  "Takes a date obj and adds one day to it."
+  (java.util.Date. (+ (* 86400 1000) (.getTime date))))
+
+(defn is-palindrome? [date]
+  "Takes a date obj and determines if it is a palindrome."
+  (let [date-str (get-date-str date)]
+    (= date-str (clojure.string/reverse date-str)))) 
+
+(defn find-palindromes [start end]
+  "Takes a start and end dates and returns all palindromes within those bounds"
+  (loop [current start
+         dates []]
+    (if (time-to-stop current end) 
+      dates
+      (recur 
+        (add-one-day current)
+        (if (is-palindrome? current) 
+          (conj dates (get-date-str current))
+          dates)))))
+
+(defn -main [& args]
+  "A clojure attempt at our palindrome test"
+  (if (two-args? args)
+    (let [start (get-date-obj (first args))
+          end (get-date-obj (last args))]
+      (if (time-to-stop start end)
+        usage
+        (find-palindromes start end)))
+    usage))
